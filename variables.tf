71b45e24174296e1950447b623dc8a736cf79d1d
0Ahmedsherif <Ahmedib0299@gmail.com>
2022-02-05 03:41:33 +0000

    add ec2 deployment config

--- variables.tf
+++ variables.tf
@@ -1,23 +1,9 @@
-variable vpc_cidr_block {
-  type        = string
-}
-
-variable subnet_cidr_block {
-  type        = string
-}
-
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
 variable env_prefix {}
-
 variable az {}
+variable region {}
+variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
 
-variable region {
-  type        = string
-}
-
-variable my_ip {
-  type        = string
-}
-
-variable instance_type {
-  type        = string
-}
